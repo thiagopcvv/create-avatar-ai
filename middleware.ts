794718aabf8691fbbecc95c8c28845154bd950e6
thiagopcvv <thiagopiresdovalle@hotmail.com>
2024-11-26 16:56:59 -0400

    feat: middleware

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { getUser } from "@/app/actions/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const user = await getUser();
+
+  // Rotas públicas
+  const publicRoutes = ["/login", "/signup", "/"];
+
+  // Caso não esteja autenticado e acesse rota protegida
+  if (!user && !publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // Caso autenticado, impedir acesso a páginas de login ou cadastro
+  if (user && publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"], // Rota que ignora assets
+};
